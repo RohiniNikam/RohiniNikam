3f71ed0e33d60b5242783794b7bf53c090bae86d
Rohini bhagwat <131174270+RohiniNikam@users.noreply.github.com>
2023-10-27 03:40:09 -0700

    Add files via upload

---
+++ arr_specific.js
@@ -0,0 +1,10 @@
+const arr = [2, 5, 9, 6];
+
+// Check if the array contains the value 5
+const indexOfValue = arr.indexOf(5);
+
+// Check if the value is found in the array by comparing the return value to -1
+const containsValue = indexOfValue !== -1;
+
+// Log the result
+console.log(containsValue); // true
